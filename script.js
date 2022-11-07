aef9026e3316e9228b32af4b076f0f235c79c6e4
ShubhamL0018 <117006661+ShubhamL0018@users.noreply.github.com>
2022-11-07 21:46:36 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,21 @@
+let will_pokemon = {
+	pikachu: {
+		species: 'Mouse Pokemon',
+		height: 0.4,
+		weight: 6
+	}
+}
+
+let shubham_pokemon = {
+	vaichu: {
+		species: 'Mouse Pokemon',
+		height: 0.8,
+		weight: 30
+	}
+}
+
+let height = will_pokemon.pikachu.height
+console.log('height:', height) 
+
+let vaichu_height = will_pokemon.pikachu.height
+console.log('height:', height) 
